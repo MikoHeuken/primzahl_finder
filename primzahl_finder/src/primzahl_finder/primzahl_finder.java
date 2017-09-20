909b2ede585883300c780a8eae20f22e538827b2
Gast1 <Gast1@DESKTOP-SV9HTJ8.Speedport_W_921V_1_44_000>
2017-09-20 23:05:05 +0200

    First commit.

---
+++ primzahl_finder/src/primzahl_finder/primzahl_finder.java
@@ -0,0 +1,22 @@
+package primzahl_finder;
+
+public class primzahl_finder {
+
+	public static void main(String[] args) {
+
+		for(int i=2; i<=100; i++){
+			
+			int x=i;
+			int w=2;
+			
+				for(int w=2; x%w>0 || x/w>1; w++){}
+				
+				int z=x/w;				/* w bekommen und x durch w teilen und dass erebniss als z deklarieren **/
+			
+				if(z==1) {
+					System.out.println(x);
+					
+			}	
+		}
+	}
+}

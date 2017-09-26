7fec496b3a0036f37dd10a39e43feee5735e9243
Gast1 <Gast1@DESKTOP-SV9HTJ8.Speedport_W_921V_1_44_000>
2017-09-26 20:24:51 +0200

    Fixed Problems

--- primzahl_finder/src/primzahl_finder/primzahl_finder.java
+++ primzahl_finder/src/primzahl_finder/primzahl_finder.java
@@ -3,23 +3,27 @@ package primzahl_finder;
 public class primzahl_finder {
 
 	public static void main(String[] args) {
-	  
-	  System.out.println("2");
+				
+		//*System.out.println("2");**//
+		
 
-		for(int i=2; i<=20; i++){
-			
-			int x=i;
-			int w;
+			for(int i=10; i<=1000; i++){
+				
+			if(i==2) {
+				System.out.println("2");
+			}
+				int x=i;
+				int w;
 			
 				for(w=2; x%w!=0 && x/w!=1; w++){}
 				
-				int z=w;				/* w bekommen und x durch w teilen und dass erebniss als z deklarieren **/
+				int z=w;				
 			
 				if(x%z!=0) {
 					System.out.println(x);
 					
 			}	
 		}
+			
 	}
 }
-//test//
